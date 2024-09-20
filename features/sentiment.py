79d963b30233497cb18140d023a1108eb1809d9d
komal8431 <162457407+komal8431@users.noreply.github.com>
2024-09-20 22:28:35 +0530

    Update sentiment.py
    added some comments.

--- features/sentiment.py
+++ features/sentiment.py
@@ -4,11 +4,11 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 # Ensure NLTK resources are downloaded
 nltk.download('vader_lexicon')
-
+#sentiment class
 class SentimentAnalyzer:
     def __init__(self):
         self.sid = SentimentIntensityAnalyzer()
-
+#To analyze the mood of the user by giving them sentiment polarity scores.
     def analyze_mood(self, user_input):
         sentiment_scores = self.sid.polarity_scores(user_input)
         if sentiment_scores['compound'] >= 0.05:
