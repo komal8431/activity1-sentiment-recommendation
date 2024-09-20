64be5df831270e47e81c0e322b67d5e2539ac194
komal8431 <raktadekomal@gmail.com>
2024-09-20 19:02:39 +0530

    added return statements to pass test cases

--- features/main.py
+++ features/main.py
@@ -16,26 +16,30 @@ def main():
     print(f"Detected mood: {mood}")
 
     # Ask user for preference
-    choice = input("Enter 1 for Series Recommedation\nEnter 2 for Songs Recommendation\nYour Input:")
+    choice = int(input("Enter 1 for Series Recommedation\nEnter 2 for Songs Recommendation\nYour Input:"))
 
     # Provide recommendations based on the user's choice
-    if choice == "1":
+    if choice == 1:
         movie_recommendations = recommender.recommend_movies(mood)
         if movie_recommendations.empty:
             print("Sorry, we couldn't find any series recommendations for your mood.")
         else:
             print("\nRecommended Series for your mood:")
             print(movie_recommendations.to_string(index=False))
-    elif choice == "2":
+        return user_input,choice
+    elif choice == 2:
         song_recommendations = recommender.recommend_songs(mood)
         if song_recommendations.empty:
             print("Sorry, we couldn't find any song recommendations for your mood.")
         else:
             print("\nRecommended Songs for your mood:")
             print(song_recommendations.to_string(index=False))
+        return user_input,choice
     else:
         print(choice)
         print("Invalid choice. Please enter 'series' or 'songs'.")
+    
+
 
 if __name__ == "__main__":
     main()
